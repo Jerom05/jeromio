c58df03f5028b4447853ffaf078d58cdf9d3f921
Jerom Ghagra <Jeromghagra@gmail.com>
2020-12-26 07:04:34 +0600

    update version2

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -10,67 +10,69 @@ import css from './css.png'
 
 const Skills = ()=>{
     return(
-        <div className="skills">
-          <div className='content'>
-            <h1>My skills</h1>
-          </div>
-
-          <div className='skills-list'>
-
-            <div className='item'>
-                <div className='item-content'>
-                    <h3 className='item-name'>JavaScript</h3>
-                    <img src ={javascript} alt='item'/>
+        <div className='skills-container'>
+            <div className="skills">
+                <div className='content'>
+                  <h1>My skills</h1>
                 </div>
+
+                <div className='skills-list'>
+
+                  <div className='item'>
+                      <div className='item-content'>
+                          <h3 className='item-name'>JavaScript</h3>
+                          <img src ={javascript} alt='item'/>
+                      </div>
+                  </div>
+
+                  <div className='item'>
+                      <div className='item-content'>
+                          <h3 className='item-name'>Node Js</h3>
+                          <img src ={nodejs} alt='item'/>
+                      </div>
+                  </div>
+
+                  <div className='item'>
+                      <div className='item-content'>
+                          <h3 className='item-name'>Express Js</h3>
+                          <img src ={express} alt='item'/>
+                      </div>
+                  </div>
+
+                  <div className='item'>
+                      <div className='item-content'>
+                          <h3 className='item-name'>React Js</h3>
+                          <img  src ={reactjs} alt='item'/>
+                      </div>
+                  </div>
+
+                  <div className='item'>
+                      <div className='item-content'>
+                          <h3 className='item-name'>Mongodb</h3>
+                          <img src ={mongodb} alt='item'/> 
+                      </div>
+
+                  </div>
+
+                  <div className='item'>
+                      <div className='item-content'>
+                          <h3 className='item-name'>CSS</h3>
+                          <img src ={css} alt='item'/> 
+                      </div>
+
+                  </div>
+
+                  <div className='item'>
+                      <div className='item-content'>
+                          <h3 className='item-name'>HTML</h3>
+                          <img src ={html} alt='item'/>
+                      </div>
+                  </div>
+
+                </div> 
             </div>
-
-            <div className='item'>
-                <div className='item-content'>
-                    <h3 className='item-name'>Node Js</h3>
-                    <img src ={nodejs} alt='item'/>
-                </div>
-            </div>
-
-            <div className='item'>
-                <div className='item-content'>
-                    <h3 className='item-name'>Express Js</h3>
-                    <img src ={express} alt='item'/>
-                </div>
-            </div>
-
-            <div className='item'>
-                <div className='item-content'>
-                    <h3 className='item-name'>React Js</h3>
-                    <img  src ={reactjs} alt='item'/>
-                </div>
-            </div>
-
-            <div className='item'>
-                <div className='item-content'>
-                    <h3 className='item-name'>Mongodb</h3>
-                    <img src ={mongodb} alt='item'/> 
-                </div>
-                
-            </div>
-
-            <div className='item'>
-                <div className='item-content'>
-                    <h3 className='item-name'>CSS</h3>
-                    <img src ={css} alt='item'/> 
-                </div>
-                
-            </div>
-
-            <div className='item'>
-                <div className='item-content'>
-                    <h3 className='item-name'>HTML</h3>
-                    <img src ={html} alt='item'/>
-                </div>
-            </div>
-
-          </div> 
-
         </div>
+        
     )
 }
 
